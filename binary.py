add89f55891e87dae96c6830d1c54df44b723db8
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-06-01 20:25:23 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,6 +1,6 @@
-num1=str(input())
-for i in range(0,len(num1)):
-    if num1[i]!='0' and num1[i]!='1':
+numb1=str(input())
+for i in range(0,len(numb1)):
+    if numb1[i]!='0' and numb1[i]!='1':
         print("no")
         break
 else:
