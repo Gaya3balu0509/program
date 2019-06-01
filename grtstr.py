2b06b11e91f5a2758b434dcf7138cc577cde3c6a
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-06-01 21:41:43 +0530

    Update grtstr.py

--- grtstr.py
+++ grtstr.py
@@ -1,7 +1,10 @@
-lis=list(map(str,input().split()))
+lis=list(input().split())
 a=lis[0]
 b=lis[1]
-if len(a)>len(b):
-    print(a)
-else:
+if len(a)==len(b):
     print(b)
+else:
+    if len(a)>len(b):
+        print(a)
+    else:
+        print(b)
