79b456111d195039b7330c7672a8dfc79b0429b6
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-06-02 20:49:07 +0530

    Update repition.py

--- repition.py
+++ repition.py
@@ -1,7 +1,4 @@
 ls,ls1=map(int,input().split())
 list1=list(map(int,input().split()))
-max1=1
-for i in list1:
-    if list1.count(i)>max1:
-        max1=list1.count(i)
+max1=list1.count(ls1)
 print(max1)
