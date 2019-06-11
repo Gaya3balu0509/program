8a30e9232fdff41a9286f930aaeb15feaa233df4
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-06-11 12:28:51 +0530

    Update sameplane.py

--- sameplane.py
+++ sameplane.py
@@ -1,7 +1,7 @@
 a,b=map(int,input().split())
 c,d=map(int,input().split())
 e,f=map(int,input().split())
-if a==c==e or b==d==f:
+if a==c==e or b==d==f or (a==b and c==d and e==f):
     print("yes")
 else:
     print("no")
