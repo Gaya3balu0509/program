387b802e9bf47160539e55b967b67859cb704730
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-06-22 16:55:38 +0530

    Rename checkk.py to checkfork.py

---
+++ checkfork.py
@@ -0,0 +1,10 @@
+inp1,inp2=map(str,input().split())
+list1=list(map(int,input().split()))
+c=0
+for i in list1:
+    if i==int(inp2):
+        c=1
+if c==1:    
+     print("yes")
+else:
+    print("no")
