ecf9f4ca58a26a798d26aa7a69bffb045b68a937
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-07-25 16:35:40 +0530

    Update repfst.py

--- repfst.py
+++ repfst.py
@@ -1,10 +1,12 @@
 a=int(input())
 l3=[]
 l=list(map(int,input().split()))
-flag=0
 for i in range(0,len(l)):
     for j in range(i+1,len(l)):
         if l[i]==l[j]:
             l3.append(j)
-m=min(l3)
-print(l[m])
+if len(l3)>0:
+    m=min(l3)
+    print(l[m])
+else:
+    print('unique')
