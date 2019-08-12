397a5e1373841a247775d5f7b2adfb99bf12e10c
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-08-12 16:54:14 +0530

    Update multiplyy.py

--- multiplyy.py
+++ multiplyy.py
@@ -1,8 +1,10 @@
 a=int(input())
 l1=list(map(int,input().split()))
-for i in l1:
+c=0
+for i in range(0,len(l1)):
     te=1
-    for j in l1:
-	    if i!=j:
-		    te=te*j
+    for j in range (0,len(l1)):
+	    if c!=j:
+             te=te*l1[j]
+    c+=1
     print(te,end=" ")
